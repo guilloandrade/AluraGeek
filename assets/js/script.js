6b01d493279479e0273befde4c753c8267f17792
guillo andrade <guilloandrade@hotmail.com>
2023-07-21 17:03:54 -0500

    corrigiendo buscaqueda responsiva

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,11 @@
 const btnLupa2 = document.querySelector(".header__lupa2");
 const btnLupa = document.querySelector(".header__lupa");
 const barraBuscar = document.querySelector(".header__buscar");
-const logo = document.querySelector(".header__logo");
-const header = document.querySelector(".header");
 
 function Lupa2() {
     barraBuscar.classList.add("miclase");
     btnLupa2.classList.add("lupaNone2");
     btnLupa.classList.add("lupaNone");
-    header.classList.add("headerBusqueda");
-    if (logo) {
-        logo.classList.add("logomargen");
-    }
 
     
 
@@ -21,10 +15,6 @@ function quitarClases() {
     barraBuscar.classList.remove("miclase");
     btnLupa2.classList.remove("lupaNone2");
     btnLupa.classList.remove("lupaNone");
-    header.classList.remove("headerBusqueda");
-    if (logo) {
-        logo.classList.remove("logomargen");
-    }
 
 }
 
